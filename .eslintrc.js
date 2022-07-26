67430f9fb018bd86a42dabe1301680dd8f8dd2be
jose <joseluis836@hotmail.com>
2022-07-26 11:24:21 -0500

    Se agregaron las etiquetas Script en _app.js para configurar el uso de google analytics

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,6 @@ module.exports = {
   ],
   rules: {
     'semi': ['error', 'always'],
-    'prettier/pretier': 0,
+    'prettier/prettier': 0,
   }
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,21 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import { Header } from '@components/Header';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
-
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XDP26E8NM8" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XDP26E8NM8');
+        `}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
